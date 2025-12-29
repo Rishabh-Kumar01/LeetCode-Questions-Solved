e926f21d165b43a22835f52c057894cfcafe97e8
Rishabh kumar <72819281+Rishabh-Kumar01@users.noreply.github.com>
2025-12-29 10:00:26 +0530

    Time: 480 ms (40.04%), Space: 177.7 MB (25.53%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    int cntSubArraySum(const vector<int> temp, const int target) {
+    int cntSubArraySum(vector<int> nums, int k) {
+        int cnt = 0;
+        
         unordered_map<int, int> m;
-        m.reserve(temp.size() + 1);
         m[0] = 1;
 
-        int cnt = 0;
         int prefixSum = 0;
 
-        for(auto i : temp) {
+        for(auto i : nums) {
             prefixSum += i;
 
-            cnt += m[prefixSum - target];
+            if(m.count(prefixSum - k)) {
+                cnt += m[prefixSum - k];
+            }
 
             m[prefixSum]++;
         }
 
         return cnt;
     }
+
     int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
         int ans = 0;
 
@@ -25,14 +28,12 @@ public:
         int n = matrix[0].size();
 
         for(int top = 0; top < m; top++) {
-
             vector<int> temp(n, 0);
 
             for(int bottom = top; bottom < m; bottom++) {
+                
 
-                for(int col = 0; col < n; col++) {
-                    temp[col] += matrix[bottom][col];
-                }
+                for(int col = 0; col < n; col++) temp[col] += matrix[bottom][col];
 
                 ans += cntSubArraySum(temp, target);
             }

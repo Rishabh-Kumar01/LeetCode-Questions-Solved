05cb16dcf712d5d77dc6687cbc792136e0cbf832
Rishabh kumar <72819281+Rishabh-Kumar01@users.noreply.github.com>
2026-01-05 11:25:15 +0530

    Time: 8 ms (10.38%), Space: 15.3 MB (11.21%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        vector<string> temp;
         stringstream ss(path);
         string token;
-
-        while(getline(ss, token, '/')) {
-            temp.push_back(token);
-        }
-        
-        int n = temp.size();
-
         stack<string> st;
 
-        for(int i = 0; i < n; i++) {
-            if(temp[i] == "" || temp[i] == ".") continue;
-            else if(temp[i] == "..") {
+        while(getline(ss, token, '/')) {
+            if(token == "" || token == ".") continue;
+            else if(token == "..") {
                 if(!st.empty()) st.pop();
-            }
-            else st.push(temp[i]);
+            } else st.push(token);
         }
-
+        
         string ans = "";
 
         while(!st.empty()) {

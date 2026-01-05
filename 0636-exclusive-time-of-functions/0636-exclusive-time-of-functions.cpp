72b84e1d644ab7ae5c4af05b9f9427a9404cff3b
Rishabh kumar <72819281+Rishabh-Kumar01@users.noreply.github.com>
2026-01-05 14:46:29 +0530

    Time: 24 ms (10.74%), Space: 27.2 MB (7.74%) - LeetHub

---
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> exclusiveTime(int n, vector<string>& logs) {
+        vector<int> ans(n,0);
+
+        stack<int> st;
+
+        int prevTime = 0;
+
+        for(int i = 0; i < logs.size(); i++) {
+            stringstream ss(logs[i]);
+            vector<string> temp;
+            string itr;
+
+            while(getline(ss, itr, ':')) {
+                temp.push_back(itr);
+            }
+
+            if(temp[1] == "start") {
+                if(!st.empty()) {
+                    int idx = st.top();
+                    ans[idx] += (stoi(temp[2]) - prevTime);
+                    prevTime = stoi(temp[2]);
+                } 
+                st.push(stoi(temp[0]));
+                
+            } else {
+                if(!st.empty()) {
+                    int idx = st.top();
+                    st.pop();
+
+                    ans[idx] += (stoi(temp[2]) - prevTime + 1);
+                    prevTime = stoi(temp[2]) + 1;
+                }
+            }
+        }
+
+        return ans;
+    }
+};

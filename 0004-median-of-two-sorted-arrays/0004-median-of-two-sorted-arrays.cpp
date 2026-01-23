8a1ebe0df2bcebc1a36fdcd804a16011f9592138
Rishabh kumar <72819281+Rishabh-Kumar01@users.noreply.github.com>
2026-01-23 14:47:59 +0530

    Time: 17 ms (5.02%), Space: 95.3 MB (44.25%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,20 +4,26 @@ public:
                int idx2, int k) {
 
         if (idx1 >= m) {
-            return nums2[idx2 + k];
+            return nums2[idx2 + k - 1];
         }
 
         if (idx2 >= n) {
-            return nums1[idx1 + k];
+            return nums1[idx1 + k - 1];
         }
 
-        if (k == 0)
+        if (k == 1)
             return min(nums1[idx1], nums2[idx2]);
 
-        if (nums1[idx1] <= nums2[idx2]) {
-            return helper(nums1, nums2, m, n, idx1 + 1, idx2, k - 1);
+        int half = k / 2;
+        int mid1 = (idx1 + half - 1< nums1.size()) ? nums1[idx1 + half - 1] : INT_MAX;
+        int mid2 = (idx2 + half - 1< nums2.size()) ? nums2[idx2 + half - 1] : INT_MAX;
+
+        if (mid1 <= mid2) {
+            // Eliminate first half of nums1
+            return helper(nums1, nums2, m, n, idx1 + half, idx2, k - half);
         } else {
-            return helper(nums1, nums2, m, n, idx1, idx2 + 1, k - 1);
+            // Eliminate first half of nums2
+            return helper(nums1, nums2, m, n, idx1, idx2 + half, k - half);
         }
     }
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
@@ -29,10 +35,12 @@ public:
         int mid = total / 2;
 
         if (total % 2 == 1) {
-            return helper(nums1, nums2, m, n, 0, 0, mid);
+            return helper(nums1, nums2, m, n, 0, 0, mid+1);
         } else {
             int mid1 = helper(nums1, nums2, m, n, 0, 0, mid);
-            int mid2 = helper(nums1, nums2, m, n, 0, 0, mid - 1);
+            int mid2 = helper(nums1, nums2, m, n, 0, 0, mid + 1);
+            cout<<"mid1 - "<<mid1<<endl;
+            cout<<"mid 2 -"<<mid2<<endl;
             return (mid1 + mid2) / 2.0;
         }
     }

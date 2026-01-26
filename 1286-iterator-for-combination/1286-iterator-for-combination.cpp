64c3f4cd55b2706fd9b565ba1f7056bc65b0a6e4
Rishabh kumar <72819281+Rishabh-Kumar01@users.noreply.github.com>
2026-01-26 21:58:12 +0530

    Time: 4 ms (64.71%), Space: 24.2 MB (55.88%) - LeetHub

---
+++ 1286-iterator-for-combination/1286-iterator-for-combination.cpp
@@ -0,0 +1,38 @@
+class CombinationIterator {
+private:
+    vector<string> combinations;
+    int index;
+
+    void helper(string& s, vector<string>& combinations, int start,
+                const int& k, string& curr) {
+        if (curr.size() == k) {
+            combinations.push_back(curr);
+            return;
+        }
+
+        for (int i = start; i < s.size(); i++) {
+            curr.push_back(s[i]);
+            helper(s, combinations, i + 1, k, curr);
+            curr.pop_back();
+        }
+    }
+
+public:
+    CombinationIterator(string characters, int combinationLength) {
+        index = 0;
+        string curr = "";
+
+        helper(characters, combinations, 0, combinationLength, curr);
+    }
+
+    string next() { return combinations[index++]; }
+
+    bool hasNext() { return index < combinations.size(); }
+};
+
+/**
+ * Your CombinationIterator object will be instantiated and called as such:
+ * CombinationIterator* obj = new CombinationIterator(characters,
+ * combinationLength); string param_1 = obj->next(); bool param_2 =
+ * obj->hasNext();
+ */

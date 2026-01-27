a438dc7539fdebd06c75ea5824f109c7193f1b08
Rishabh kumar <72819281+Rishabh-Kumar01@users.noreply.github.com>
2026-01-27 12:34:56 +0530

    Time: 6 ms (6.6%), Space: 12.7 MB (6.69%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+public:
+    void helper(vector<int> &nums, vector<int> curr, vector<vector<int>> &ans, vector<bool> used){
+        if(curr.size() == nums.size()) {
+            ans.push_back(curr);
+            return;
+        }
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(used[i]) continue;
+
+            curr.push_back(nums[i]);
+            used[i] = true;
+            helper(nums, curr, ans, used);
+
+            curr.pop_back();
+            used[i] = false;
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        
+        vector<bool> used(nums.size(), false);
+
+        helper(nums, curr, ans, used);
+
+        return ans;
+    }
+};
